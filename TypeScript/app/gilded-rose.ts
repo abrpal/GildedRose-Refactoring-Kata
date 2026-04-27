bac50721f235eb2d78fc46fe0498f1917408b1a9
Abraham <a.palomino.gz@gmail.com>
2026-04-27 17:55:03 -0600

    extracted increaseQuality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,9 +19,21 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      
       const isAgedBrie = this.items[i].name === 'Aged Brie';
       const isBackstagePasses = this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert';
       const isSulfuras = this.items[i].name === 'Sulfuras, Hand of Ragnaros';
+
+
+      // if(isAgedBrie){
+      //   if (this.items[i].quality < 50)
+      // }else if(isBackstagePasses){
+
+      // }else if(isSulfuras){
+
+      // }else{
+        
+      // }
 
       if (!isAgedBrie && !isBackstagePasses) {
         if (this.items[i].quality > 0) {
@@ -29,26 +41,29 @@ export class GildedRose {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
+      } else if (this.items[i].quality < 50) {
+          this.increaseQuality(i);
+          
           if (isBackstagePasses) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.increaseQuality(i);
+
+                if (this.items[i].sellIn < 6) this.increaseQuality(i);
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
         }
       }
+
+
+
+
+
       if (!isSulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
+      
       if (this.items[i].sellIn < 0) {
         if (!isAgedBrie) {
           if (!isBackstagePasses) {
@@ -62,12 +77,17 @@ export class GildedRose {
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+            this.increaseQuality(i);
           }
         }
       }
     }
 
     return this.items;
+  }
+
+
+  private increaseQuality(itemIndex){
+    this.items[itemIndex].quality = this.items[itemIndex].quality + 1
   }
 }
