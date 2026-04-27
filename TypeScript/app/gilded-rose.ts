ecf2ead868f13172f7fac7d8b6739a1cade67d76
Abraham <a.palomino.gz@gmail.com>
2026-04-27 17:38:18 -0600

    Defined isAgedBrie, isBackstagePasses and isSulfuras variables.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,16 +19,20 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      const isAgedBrie = this.items[i].name === 'Aged Brie';
+      const isBackstagePasses = this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert';
+      const isSulfuras = this.items[i].name === 'Sulfuras, Hand of Ragnaros';
+
+      if (!isAgedBrie && !isBackstagePasses) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (!isSulfuras) {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (isBackstagePasses) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,14 +46,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (!isSulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (!isAgedBrie) {
+          if (!isBackstagePasses) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (!isSulfuras) {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
