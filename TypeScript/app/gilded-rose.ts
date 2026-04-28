073c6f72dc60ac7d3b88591a4f835ce737523c18
Abraham <a.palomino.gz@gmail.com>
2026-04-27 18:20:07 -0600

    simplified logic to increase quality in backstage passes.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -2,7 +2,7 @@ export class Item {
   name: string;
   sellIn: number;
   quality: number;
-
+  
   constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
@@ -12,49 +12,53 @@ export class Item {
 
 export class GildedRose {
   items: Array<Item>;
-
+  
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
-
+  
   updateQuality() {
+    const QUALITY_MAX : number = 50;
+    
+
     for (let i = 0; i < this.items.length; i++) {
       
       const isAgedBrie = this.items[i].name === 'Aged Brie';
       const isBackstagePasses = this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert';
       const isSulfuras = this.items[i].name === 'Sulfuras, Hand of Ragnaros';
-
+      
       if (isBackstagePasses) {
-        if (this.items[i].quality < 50){ 
-          this.increaseQuality(i);
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.increaseQuality(i);
-
-                if (this.items[i].sellIn < 6) this.increaseQuality(i);
-              }
-            }
-          }
+        if (this.items[i].sellIn < 6){
+          this.increaseQuality(i, 3);
+        }else if(this.items[i].sellIn < 11){ 
+          this.increaseQuality(i, 2);
         }else{
-          if (!isAgedBrie) {
-            if (this.items[i].quality > 0) {
-              if (!isSulfuras) {
-                this.decreaseQuality(i)
-              }
-            }
-          } else if (this.items[i].quality < 50) {
-            this.increaseQuality(i);
-          }
+          this.increaseQuality(i,1);
         }
+        
+        if (this.items[i].quality > QUALITY_MAX) this.items[i].quality = QUALITY_MAX;
 
 
-
-
-
+      }else{
+        if (!isAgedBrie) {
+          if (this.items[i].quality > 0) {
+            if (!isSulfuras) {
+              this.decreaseQuality(i)
+            }
+          }
+        } else if (this.items[i].quality < QUALITY_MAX) {
+          this.increaseQuality(i,1);
+        }
+      }
+      
+      
+      
+      
+      
       if (!isSulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-
+      
       
       if (this.items[i].sellIn < 0) {
         if (!isAgedBrie) {
@@ -68,21 +72,21 @@ export class GildedRose {
             this.items[i].quality = 0;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.increaseQuality(i);
+          if (this.items[i].quality < QUALITY_MAX) {
+            this.increaseQuality(i,1);
           }
         }
       }
     }
-
+    
     return this.items;
   }
-
-
-  private increaseQuality(itemIndex){
-    this.items[itemIndex].quality = this.items[itemIndex].quality + 1
+  
+  
+  private increaseQuality(itemIndex, increaseAmount){
+    this.items[itemIndex].quality = this.items[itemIndex].quality + increaseAmount;
   }
-
+  
   private decreaseQuality(itemIndex){
     this.items[itemIndex].quality = this.items[itemIndex].quality - 1
   }
